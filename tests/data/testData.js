c73a1526488aa164e4dffc53834ec49017fc6392
tm57 <tmatate.act@gmail.com>
2016-01-18 02:30:46 +0100

    first commit

---
+++ tests/data/testData.js
@@ -0,0 +1,26 @@
+var jscvDemoData =
+  {
+    "firstName" : "Jon",
+    "lastName" : "Doe",
+    "middleName" : "Awesome",
+    "avatar" : "/assets/img/jdoe.jpeg",
+    "email" : "johndoe@email.com",
+    "phoneNumber" : "+123 456 789",
+    "cellPhoneNumber" : "+123 456 789",
+    "physicalAddress" : "Youkon Strasse\n12345 Youkon City State, YC\nUnited States",
+    "dateOfBirth" : "12/12/2016",
+    "workExperience" : [
+        {"sectionTitle": 'Youkon High School 1',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"},
+        {"sectionTitle": 'Youkon High School 2',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"},
+        {"sectionTitle": 'Youkon High School 3',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"}],
+    "education" : [
+        {"sectionTitle": 'Youkon High School 1',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"},
+        {"sectionTitle": 'Youkon High School 2',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"},
+        {"sectionTitle": 'Youkon High School 3',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"}],
+    "extracurricularActivities" : [
+        {"sectionTitle": 'Youkon High School 1',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"},
+        {"sectionTitle": 'Youkon High School 2',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"}],
+    "skillsAndAchievements" : [
+        {"sectionTitle": 'Youkon High School 1',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"},
+        {"sectionTitle": 'Youkon High School 2',"sectionDetailedDescription":'I was vice headboy and collected several awesome awards',"startDate":"01/01/1999","endDate":"01/01/2015"}]
+  };
